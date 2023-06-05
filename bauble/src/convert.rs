c37af12a8dc58f6ed79e0df115eeaa747369bae2
Isse <isidornie@gmail.com>
2023-06-05 12:10:26 +0200

    add usize and isize

--- bauble/src/convert.rs
+++ bauble/src/convert.rs
@@ -200,8 +200,8 @@ macro_rules! impl_nums {
 }
 
 impl_nums! {
-    u8, u16, u32, u64, u128,
-    i8, i16, i32, i64, i128,
+    u8, u16, u32, u64, u128, usize,
+    i8, i16, i32, i64, i128, isize,
     f32, f64,
 }
 
