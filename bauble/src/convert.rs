ef734e8a79f93203a886f65ab263d3c2520f8413
Joshua Barretto <joshua.s.barretto@gmail.com>
2023-11-01 19:37:29 +0000

    Simplified impl_tuple

--- bauble/src/convert.rs
+++ bauble/src/convert.rs
@@ -445,7 +445,12 @@ impl<'a> FromBauble<'a> for () {
 }
 
 macro_rules! impl_tuple {
-    ($($ident:ident),+) => {
+    () => {};
+    ($head:ident $($X:ident)*) => {
+        impl_tuple!($($X)*);
+        impl_tuple!(~ $head $($X)*);
+    };
+    (~ $($ident:ident)+) => {
         impl<'a, A: BaubleAllocator<'a>, $($ident: FromBauble<'a, A>),*> FromBauble<'a, A> for ($($ident),*,) {
             const INFO: TypeInfo<'static> = TypeInfo::Kind(ValueKind::Tuple);
 
@@ -494,22 +499,7 @@ macro_rules! impl_tuple {
     };
 }
 
-impl_tuple!(T0);
-impl_tuple!(T0, T1);
-impl_tuple!(T0, T1, T2);
-impl_tuple!(T0, T1, T2, T3);
-impl_tuple!(T0, T1, T2, T3, T4);
-impl_tuple!(T0, T1, T2, T3, T4, T5);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15);
+impl_tuple!(T0 T1 T2 T3 T4 T5 T6 T7 T8 T9 T10 T11 T12 T13 T14 T15);
 
 impl<'a, A: BaubleAllocator<'a>, T: FromBauble<'a, A>, const N: usize> FromBauble<'a, A>
     for [T; N]
