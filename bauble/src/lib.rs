b72674a1c4c296010d96799b9c7bd212d4c0f41a
Imbris <imbrisf@gmail.com>
2025-09-24 06:29:54 -0400

    Add notes and make minor improvements to bauble_test macro. Including testing if extra objects are produced over the expected values

--- bauble/src/lib.rs
+++ bauble/src/lib.rs
@@ -72,10 +72,10 @@ pub mod private {
 #[allow(missing_docs)]
 #[macro_export]
 macro_rules! bauble_test {
-    ( [$($ty:ty),* $(,)?] $source:literal [$($expr:expr),* $(,)?]) => {
-        $crate::bauble_test!(__TEST_CTX [$($ty),*] $source [$($expr),*])
+    ( [$($ty:ty),* $(,)?] $source:literal [$($test_value:expr),* $(,)?]) => {
+        $crate::bauble_test!(__TEST_CTX [$($ty),*] $source [$($test_value),*])
     };
-    ($ctx_static:ident [$($ty:ty),* $(,)?] $source:literal [$($expr:expr),* $(,)?]) => {
+    ($ctx_static:ident [$($ty:ty),* $(,)?] $source:literal [$($test_value:expr),* $(,)?]) => {
         static $ctx_static: std::sync::OnceLock<std::sync::RwLock<$crate::BaubleContext>> = std::sync::OnceLock::new();
         {
             let file_path = $crate::path::TypePath::new("test").unwrap();
@@ -91,14 +91,15 @@ macro_rules! bauble_test {
                 std::sync::RwLock::new(ctx)
             });
 
+            // Test initial parsing from source
             let (objects, errors) = ctx.write().unwrap().load_all();
 
             if !errors.is_empty() {
                 $crate::print_errors(Err::<(), _>(errors), &ctx.read().unwrap());
-
                 panic!("Error converting");
             }
 
+            // Test round-trip of objects through source format
             let re_source = $crate::display_formatted(objects.as_slice(), ctx.read().unwrap().type_registry(), &$crate::DisplayConfig {
                 ..$crate::DisplayConfig::default()
             });
@@ -107,29 +108,35 @@ macro_rules! bauble_test {
 
             if !errors.is_empty() {
                 $crate::print_errors(Err::<(), _>(errors), &ctx.read().unwrap());
-
-                println!("{re_source}");
-
+                eprintln!("{re_source}");
                 panic!("Error re-converting");
             }
 
+            assert_eq!(objects, re_objects);
+
+            // Test that original parsed objects and round-trip objects convert into typed values
+            // that match the provided test values.
             let compare_objects = |mut objects: Vec<$crate::Object>| {
                 let mut objects = objects.into_iter();
 
                 $(
                     let value = objects.next().expect("Not as many objects as test expr in bauble test?");
-                    let mut read_value = $expr;
-                    let test_value = ::std::mem::replace(&mut read_value, $crate::print_errors(Bauble::from_bauble(value.value, &::bauble::DefaultAllocator), &ctx.read().unwrap()).unwrap());
+                    // Infer type for `read_value` to be the same as `test_value`.
+                    let [test_value, read_value] = [
+                        $test_value,
+                        $crate::print_errors(
+                            $crate::Bauble::from_bauble(value.value, &$crate::DefaultAllocator),
+                            &ctx.read().unwrap()
+                        ).unwrap(),
+                    ];
 
-
-                    assert_eq!(
-                        read_value,
-                        test_value,
-                    );
+                    assert_eq!(read_value, test_value);
                 )*
-            };
 
-            assert_eq!(objects, re_objects);
+                if objects.next().is_some() {
+                    panic!("More objects in bauble test than test expr?");
+                }
+            };
 
             compare_objects(objects);
             compare_objects(re_objects);
